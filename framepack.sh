7dcbb56096ffcf2f2ba842e57808c60336a4bd4f
E Web Solutions <122236544+ewebsolution-dev@users.noreply.github.com>
2025-06-16 01:31:07 +0200

    Update framepack.sh

--- framepack.sh
+++ framepack.sh
@@ -6,4 +6,11 @@ set -eo pipefail
 # Activate the main virtual environment
 . /venv/main/bin/activate
 
-git clone https://github.com/lllyasviel/FramePack ./
+git clone https://github.com/lllyasviel/FramePack /workspace/
+
+# Reconfigure the instance portal
+rm -f /etc/portal.yaml
+export PORTAL_CONFIG="localhost:1111:11111:/:Instance Portal|localhost:7860:760:/Framepack"
+
+# Reload Supervisor
+supervisorctl reload
