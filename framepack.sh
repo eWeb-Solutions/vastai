a915201852498df6cb99a4fde407e57696d28d5a
E Web Solutions <122236544+ewebsolution-dev@users.noreply.github.com>
2025-06-16 12:51:21 +0200

    Update framepack.sh

--- framepack.sh
+++ framepack.sh
@@ -8,9 +8,22 @@ set -eo pipefail
 
 git clone https://github.com/lllyasviel/FramePack /workspace/
 
+# Set up any additional services
+cat <<EOF > /etc/supervisor/conf.d/framepack.conf
+
+EOF
+
+cat <<EOF > /opt/supervisor-scripts/framepack.sh
+#!/bin/bash
+echo \$PWD
+echo $PWD
+EOF
+
+chmod +x /opt/supervisor-scripts/framepack.sh
+
 # Reconfigure the instance portal
 rm -f /etc/portal.yaml
-export PORTAL_CONFIG="localhost:1111:11111:/:Instance Portal|localhost:7860:760:/Framepack"
+export PORTAL_CONFIG="localhost:8002:18002:/hello:MyApp|localhost:1111:11111:/:Instance Portal|localhost:8080:18080:/:Jupyter|localhost:8080:8080:/terminals/1:Jupyter Terminal|localhost:8384:18384:/:Syncthing|localhost:6006:16006:/:Tensorboard|localhost:7860:7860:/:Framepack"
 
 # Reload Supervisor
 supervisorctl reload
